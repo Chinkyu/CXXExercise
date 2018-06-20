cc6fefb778c12e7e35d4b120528bc78fd45b5d4c
bamtori <bamtori@hotmail.com>
2018-06-20 22:57:43 +0900

    0620_2

--- MyProfessionalC++/MyProfessionalC++/StringTest1.cpp
+++ MyProfessionalC++/MyProfessionalC++/StringTest1.cpp
@@ -5,17 +5,33 @@ using namespace std;
 
 
 void StringTest1() {
-	float f1 = 3.14;
-	string s1 = to_string(f1);
+//	float f1 = 3.14;
+//	string s1 = to_string(f1);
 
-	cout << s1 << endl;
+//	cout << s1 << endl;
+}
+
+void stringTest2() {
+	string s2;
+
+	s2.resize(3);
+	s2[2] = '1';
+
+	cout << s2 << endl;
+
+	s2.resize(2);
+	cout << s2 << endl;
+
+	s2.resize(10);
+	cout << s2 << endl;
 }
 
 
 int main() {
 	int i;
 
-	StringTest1();
+//	StringTest1();
+	stringTest2();
 
 	cin >> i ;
 
