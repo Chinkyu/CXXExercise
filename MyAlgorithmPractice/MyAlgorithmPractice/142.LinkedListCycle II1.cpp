a7779c3658973f077ddba7a615427949bb5330ae
bamtori <bamtori@hotmail.com>
2018-07-26 10:09:39 +0900

    0726

--- MyAlgorithmPractice/MyAlgorithmPractice/142.LinkedListCycle II1.cpp
+++ MyAlgorithmPractice/MyAlgorithmPractice/142.LinkedListCycle II1.cpp
@@ -23,6 +23,30 @@ struct ListNode {
 
 class Solution {
 public:
+	ListNode * getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *p, *q;
+
+		if (headA == NULL || headB == NULL) return NULL;
+
+		p = q = headA;
+
+		p = p->next ? p->next : headB;
+		while (p = p->next ? p->next : headB) {
+			// Has a loop been found.
+			if ((p = p->next ? p->next : headB) == (q = q->next ? q->next : headB)) {
+				// Move advance head and fast by a single step at a time. 
+				// When they meet, it is the start of the cycle.
+				while (p != headA) {
+					p = p->next;
+					headA = headA->next;
+				}
+				return headA;
+			}
+		}
+		return NULL;
+
+	}
+
 	ListNode *detectCycle(ListNode *head) {
 		ListNode *p, *q;
 
--- MyAlgorithmPractice/MyAlgorithmPractice/160.IntersectionOfTwoLinkedLists.cpp
+++ MyAlgorithmPractice/MyAlgorithmPractice/160.IntersectionOfTwoLinkedLists.cpp
@@ -1,9 +1,30 @@
-#if 0
-//
-// ERROR ....
-// Answer : No intersection, ERROR: linked structure was modified.
-// => Just change value.... => make this error
-// => need to find solution that doesn't change value also. 
+// From comment - It is good.... 
+/*The solution is based on 142. Linked List Cycle II.
+The Basic idea is make the first list(headA) a circle by link the end to the beginning.If there is a intersection, then there must be a circle in listB.The start pointer of the circle is the intersection pointer.Therefore, this question becomes 142. Linked List Cycle II.All you need to do is to find the start pointer of the circle.
+Don't forget to break the circle when the algorithm are finished.
+
+
+class Solution {
+public:
+	ListNode * getIntersectionNode(ListNode *headA, ListNode *headB) {
+		if (headA == nullptr)
+			return nullptr;
+		//First: make a circle
+		ListNode* end = headA;
+		while (end->next != nullptr) end = end->next;
+		end->next = headA;
+
+		//Second: Copy the code of Question 142: Linked List Cycle II
+		ListNode* res = yourSolutionOfQuestion142(headB);
+
+		//Third: Break the circle.
+		end->next = nullptr;
+
+		return res;
+
+	}
+};
+*/
 #include <iostream>
 
 using namespace std;
@@ -25,38 +46,46 @@ struct ListNode {
 
 class Solution {
 public:
-	ListNode * getIntersectionNode(ListNode *headA, ListNode *headB) {
-		// init val for each ListNode
+	
+	ListNode * detectCycle(ListNode *head) {
 		ListNode *p, *q;
 
-		p = headA;
-		while (p != NULL) {
-			p->val = 0;
-			p = p->next;
-		}
+		if (head == NULL) return NULL;
 
-		p = headB;
-		while (p != NULL) {
-			p->val = 0;
-			p = p->next;
-		}
-
-		p = headA;
-		while (p != NULL && p->next != NULL) {
-			p->next->val++;
-No intersection, ERROR: linked structure was modified.			p = p->next;
-		}
-
-		p = headB;
-		while (p != NULL && p->next != NULL) {
-			p->next->val++;
-			if (p->next->val >= 2) return p->next;
-			p = p->next;
+		p = q = head;
+		while (p && (p = p->next)) {
+			// Has a loop been found.
+			if ((p = p->next) == (q = q->next)) {
+				// Move advance head and fast by a single step at a time.
+				// When they meet, it is the start of the cycle.
+				while (p != head) {
+					p = p->next;
+					head = head->next;
+				}
+				return head;
+			}
 		}
 		return NULL;
 	}
-};
 
+	ListNode * getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *p, *q;
+
+		if (headA == NULL || headB == NULL) return NULL;
+
+		p = headA;
+		while (p->next != NULL) p = p->next;
+		p->next = headA;
+
+		q = detectCycle(headB);
+
+		// Back p->next to NULL
+		p->next = NULL;
+		
+		return q;
+	}
+
+};
 
 int main() {
 	Solution sol;
