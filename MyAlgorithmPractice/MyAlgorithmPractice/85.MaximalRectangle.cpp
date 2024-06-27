7d27e31d4925bc78627312a01b9c2b5cfc2928e0
bamtori <bamtori@hotmail.com>
2024-06-26 18:23:54 -0700

    1
    1

--- MyAlgorithmPractice/MyAlgorithmPractice/85.MaximalRectangle.cpp
+++ MyAlgorithmPractice/MyAlgorithmPractice/85.MaximalRectangle.cpp
@@ -20,37 +20,49 @@
 using namespace std;
 
 class Solution {
-    func maximalRectangle(_ matrix : [[Character]] ) -> Int {
-        guard !matrix.isEmpty, let cols = matrix.first ? .count else { return 0 }
+private:
+    int area(vector<int>& heights)
+    {
+        int n = heights.size();
+        int ans = 0;
+        stack<int>st;
+        for (int i = 0; i <= n; ++i)
+        {
+            while (!st.empty() and (i == n || heights[st.top()] >= heights[i]))
+            {
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if (st.empty()) width = i;
+                else width = i - st.top() - 1;
 
-        var maxArea = 0
-            var heights = [Int](repeating: 0, count : cols + 1)
-
-            for row in matrix{
-                for j in 0.. < cols {
-                    if row[j] == "1" {
-                        heights[j] += 1
-                    }
-     else {
-      heights[j] = 0
-  }
-}
-
-var stack = [-1]
-for j in 0...cols {
-    while let last = stack.last, last != -1, heights[j] < heights[last] {
-        stack.removeLast()
-        let height = heights[last]
-        let width = j - 1 - (stack.last ? ? -1)
-        maxArea = max(maxArea, height * width)
-    }
-    stack.append(j)
-}
+                ans = max(ans, width * height);
             }
+            st.push(i);
 
-                return maxArea
+        }
+        return ans;
     }
-}
+public:
+    int maximalRectangle(vector<vector<char>>& matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>heights(m, 0);
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (matrix[i][j] == '1') heights[j]++;
+                else heights[j] = 0;
+            }
+            ans = max(ans, area(heights));
+        }
+        return ans;
+
+    }
+};
 
 int main() {
     char c;
