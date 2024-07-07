3f71e5ed33beb6e4f0469c7bf07c03808be9db76
bamtori <bamtori@hotmail.com>
2024-07-07 08:56:21 -0700

    1
    1

---
+++ MyAlgorithmPractice/MyAlgorithmPractice/124.BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,62 @@
+﻿// 답봤음... : 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root, int& maxi) {
+        if (root == NULL) {
+            return 0;
+        }
+
+        int lh = max(0, height(root->left, maxi));
+        int rh = max(0, height(root->right, maxi));
+
+        maxi = max(maxi, lh + rh + root->val);
+        return root->val + max(lh, rh);
+    }
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN;
+        height(root, res);
+        return res;
+    }
+};
+
+int main() {
+    char c;
+    Solution sol;
+
+    vector<int> heights = { 2,1,5,6,2,3 };
+
+    cout << sol.largestRectangleArea(heights);
+
+    cin >> c;
+}
