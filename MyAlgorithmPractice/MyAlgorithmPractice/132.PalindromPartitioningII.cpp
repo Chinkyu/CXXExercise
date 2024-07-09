7ee242928c3cc7d9d1cd8ffa751f85a592e8eb67
bamtori <bamtori@hotmail.com>
2024-07-09 09:43:19 -0700

    1
    1

---
+++ MyAlgorithmPractice/MyAlgorithmPractice/132.PalindromPartitioningII.cpp
@@ -0,0 +1,48 @@
+﻿// 답봤음... : 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> cut(n + 1, 0);  // number of cuts for the first k characters
+        for (int i = 0; i <= n; i++) cut[i] = i - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j]; j++) // odd length palindrome
+                cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j]);
+
+            for (int j = 1; i - j + 1 >= 0 && i + j < n && s[i - j + 1] == s[i + j]; j++) // even length palindrome
+                cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j + 1]);
+        }
+        return cut[n];
+    }
+};
+
+int main() {
+    char c;
+    Solution sol;
+
+    vector<int> heights = { 2,1,5,6,2,3 };
+
+    cout << sol.largestRectangleArea(heights);
+
+    cin >> c;
+}
