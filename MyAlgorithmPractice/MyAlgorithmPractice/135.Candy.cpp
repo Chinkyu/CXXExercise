bc7ed74a692d7854f757829e595a7f50ea34e8e6
bamtori <bamtori@hotmail.com>
2024-07-10 18:34:54 -0700

    1
    1

---
+++ MyAlgorithmPractice/MyAlgorithmPractice/135.Candy.cpp
@@ -0,0 +1,64 @@
+﻿// 답봤음... : 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int candy = n, i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
+            }
+
+            //For increasing slope
+            int peak = 0;
+            while (ratings[i] > ratings[i - 1]) {
+                peak++;
+                candy += peak;
+                i++;
+                if (i == n) return candy;
+            }
+
+            //For decreasing slope
+            int valley = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley); //Keep only the higher peak
+        }
+        return candy;
+    }
+};
+
+int main() {
+    char c;
+    Solution sol;
+
+    vector<int> heights = { 2,1,5,6,2,3 };
+
+    cout << sol.largestRectangleArea(heights);
+
+    cin >> c;
+}
