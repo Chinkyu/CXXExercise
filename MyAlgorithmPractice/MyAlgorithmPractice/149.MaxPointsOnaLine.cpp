5fc18fa3d9fa69b9dc1254635cc625967211e662
bamtori <bamtori@hotmail.com>
2024-07-14 12:11:34 -0700

    1
    1

---
+++ MyAlgorithmPractice/MyAlgorithmPractice/149.MaxPointsOnaLine.cpp
@@ -0,0 +1,62 @@
+﻿// 답봤음... : back trace ??? 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        if (n <= 2) // if the size is 2 then return 2
+            return n;
+
+
+        int maxi = 2;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int count = 2;
+                for (int k = 0; k < n; k++)
+                {
+                    if (k != i && k != j)
+                    {
+                        if ((points[j][1] - points[i][1]) * (points[i][0] - points[k][0]) == (points[i][1] - points[k][1]) * (points[j][0] - points[i][0]))
+                            count++;
+                    }
+                }
+                maxi = max(maxi, count);
+            }
+        }
+        return maxi;
+    }
+};
+
+
+int main() {
+    char c;
+    Solution sol;
+
+    vector<int> heights = { 2,1,5,6,2,3 };
+
+    cout << sol.largestRectangleArea(heights);
+
+    cin >> c;
+}
---
+++ MyAlgorithmPractice/MyAlgorithmPractice/154.FindMinimuminRotatedSortedArrayII.cpp
@@ -0,0 +1,54 @@
+﻿// 답봤음... : what binary search????
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& v) {
+        int l = 0, h = v.size() - 1;
+        while (l < h)
+        {
+            int m = (l + h) / 2;
+            if (v[m] > v[h]) {
+                l = m + 1;
+            }
+            else if (v[l] < v[m])
+            {
+                h = m - 1;
+            }
+            else
+            {
+                h--;
+            }
+        }
+        return v[l];
+    }
+};
+
+int main() {
+    char c;
+    Solution sol;
+
+    vector<int> heights = { 2,1,5,6,2,3 };
+
+    cout << sol.largestRectangleArea(heights);
+
+    cin >> c;
+}
