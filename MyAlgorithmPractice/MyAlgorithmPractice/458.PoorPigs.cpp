6c9f76458c551ae248eaa36d5fb70287e244ec42
bamtori <bamtori@hotmail.com>
2024-08-21 17:55:56 -0700

    1
    1

---
+++ MyAlgorithmPractice/MyAlgorithmPractice/458.PoorPigs.cpp
@@ -0,0 +1,46 @@
+// ¥‰∫√¿Ω : binary logic
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int poorPigs(int buckets, int timeDetect, int timeTest) {
+        return ceil(log2(buckets) / log2(int(timeTest / timeDetect) + 1));
+    }
+};
+
+int main() {
+    Codec sol;
+    char c;
+
+    TreeNode* r = new TreeNode(1);
+    r->left = new TreeNode(2);
+    r->right = new TreeNode(3);
+    r->right->left = new TreeNode(4);
+    r->right->right = new TreeNode(5);
+
+
+    string ans = sol.serialize(r);
+    cout << ans;
+
+    TreeNode* root = sol.deserialize(ans);
+
+    cin >> c;
+}
