bdc9f22827d8f966862a0494c4654e4f51b0167c
bamtori <bamtori@hotmail.com>
2025-01-19 11:39:16 -0800

    1
    1

--- MyAlgorithmPracticeII/MyAlgorithmPracticeII/3305.CountofSubstringsContainingEveryVowelandKConsonatsI.cpp
+++ MyAlgorithmPracticeII/MyAlgorithmPracticeII/3305.CountofSubstringsContainingEveryVowelandKConsonatsI.cpp
@@ -19,32 +19,39 @@
 
 using namespace std;
 
-int countOfSubstrings(string s, int k) {
-    int ans = 0;
+class Solution {
+public:
+    int countOfSubstrings(string word, int k) {
+        int l = word.size();
 
-    for (int i = 0; i < s.size(); i++) {
-        int a = 0, e = 0, m = 0, o = 0, u = 0, c = 0;
-        for (int j = i; j < s.size(); j++) {
-            if (s[j] == 'a') a++;
-            else if (s[j] == 'e') e++;
-            else if (s[j] == 'i') m++;
-            else if (s[j] == 'o') o++;
-            else if (s[j] == 'u') u++;
-            else c++;
+        int ans = 0;
+        for (int id = 0; id < l; ++id) {
+            int a = 0, e = 0, i = 0, o = 0, u = 0, c = 0;
+            for (int j = id; j < l; ++j) {
 
-            if (a and e and m and o and u and c == k) ans++;
+                if (word[j] == 'a') a++;
+                else if (word[j] == 'e') e++;
+                else if (word[j] == 'i') i++;
+                else if (word[j] == 'o') o++;
+                else if (word[j] == 'u') u++;
+                else c++;
+
+                if (a > 0 && e > 0 && i > 0 && o > 0 && u > 0 && c == k) {
+                    ans++;
+                }
+            }
         }
-    }
 
-    return ans;
-}
+        return ans;
+    }
+};
 
 int main() {
     char c;
     Solution sol;
-    vector<int> nums = { 4,2,9,5,3 };
+    string word = "aeioqq";
 
-    cout << sol.maximumPrimeDifference(nums);
+    cout << sol.countOfSubstrings(word, 1);
 
     cin >> c;
 }
