07b8007d19f3ffba84c99b80defafcc3b0e49422
bamtori <bamtori@hotmail.com>
2025-04-08 20:04:20 -0700

    1
    1

---
+++ MyAlgorithmPracticeII/MyAlgorithmPracticeII/343.IntegerBreak.cpp
@@ -0,0 +1,47 @@
+//  ¥‰∫√¿Ω : 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+#include <functional>
+
+using namespace std;
+
+public class Solution {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int product = 1;
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
+        }
+        product *= n;
+
+        return product;
+    }
+}
+
+int main() {
+    char c;
+    Solution sol;
+    vector<int> arr = { 1, 3, 5 };
+
+
+    cout << sol.numOfSubarrays(arr);
+
+    cin >> c;
+}
