8ec9e3192875ecd5e892ebfe8b8681eb85f57464
bamtori <bamtori@hotmail.com>
2025-04-19 10:40:13 -0700

    1
    1

--- MyAlgorithmPracticeII/MyAlgorithmPracticeII/388.LongestAbsoluteFilePath.cpp
+++ MyAlgorithmPracticeII/MyAlgorithmPracticeII/388.LongestAbsoluteFilePath.cpp
@@ -1,4 +1,4 @@
-//  답봣음 :  \t \n 으로 처리해서 getline으로 못하고 token 모아야함.. 
+//  ok
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -21,38 +21,6 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthLongestPath(string input) {
-        stringstream ss(input);
-        string data;
-        // this map will store the len upto previous dir for the current explored path 
-        unordered_map<int, int>len;
-        int res = 0;
-
-        while (getline(ss, data, '\n'))
-        {
-            // data will contain string like dir, /tsubdir1, /tsubdir2....etc
-            int numTabs = 0;   //count no of tabs
-
-            while (data[numTabs] == '\t') numTabs++;
-
-            if (numTabs == 0)
-                len[numTabs] = data.size(); //root directory
-            else {
-                // length upto previous dir + len("/") + no of chars after tabs for current filename/ dirname
-                len[numTabs] = len[numTabs - 1] + 1 + data.substr(numTabs).size();   // added 1 for '/'
-            }
-
-            // If it's a file then update the res 
-            if (data.find('.') != string::npos)
-                res = max(res, len[numTabs]);
-        }
-
-        return res;
-    }
-};
-
-class _Solution {
-public:
 
     bool isFile(string& s) {
 
@@ -67,21 +35,23 @@ public:
         unordered_map<int, string> path;
 
         istringstream iss(input);
+        string line;
         string token;
 
-        int depth = 0;
+        //int depth = 0;
         int max_lengh = 0;
         //getline(iss, token, '/');  // for first /
 
-        while (getline(iss, token, '\\')) {
+        while (getline(iss, line, '\n')) {
 
-            if (token == "t") {
+            int depth = 0;
+            int idx = 0;
+            while (line[idx] == '\t') {
                 depth++;
-            }
-            else if (token == "n") {
-                depth = 0;
+                idx++;
             }
 
+            token = line.substr(idx);
             if (isFile(token)) {
                 int pl = 0;
                 for (int i = 0; i < depth; ++i) {
@@ -96,21 +66,18 @@ public:
             }
         }
 
-    return max_lengh;
+        return max_lengh;
     }
 };
 
 int main() {
     char c;
     Solution sol;
-    string input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+    string input = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"; 
+    //"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
 
 
-    vector<int> ans = sol.getStrongest(arr, 2);
-
-    for (auto it : ans) {
-        cout << it << " ";
-    }
+    cout << sol.lengthLongestPath(input);
 
     cin >> c;
 }
