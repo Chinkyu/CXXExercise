ddc67c43c62260a8d7b81c414ddc59ba03444aa2
bamtori <bamtori@hotmail.com>
2025-11-14 18:41:43 -0800

    1
    1

---
+++ MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/2747.CountZeroRequestServers.cpp
@@ -0,0 +1,85 @@
+// see answer : sliding window
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+
+    //Please upvote if you understood the solution and find it intuitive.
+
+    vector<int> countServers(int n, vector<vector<int>>& logs, int x, vector<int>& queries) {
+        int m = logs.size();
+        vector<pair<int, int>> vp;
+        for (auto it : logs) vp.push_back({ it[1],it[0] }); //time,serverid
+        sort(vp.begin(), vp.end()); //sort on basis of time
+
+        int q = queries.size();
+        unordered_map<int, int> mp;//to store the unique server ids in the current window
+
+        vector<int> ans(q, 0);
+        vector<pair<int, int>> time(q);
+        //store the indices of queries to store the answer in correct manner
+        for (int i = 0; i < q; i++) time[i] = { queries[i],i }; //time,index
+        sort(time.begin(), time.end()); //sort on basis of time
+
+        int i = 0, j = 0; //i is the start of window and j is the end of window
+        for (auto tm : time)
+        {
+            int curtime = tm.first;
+            int ind = tm.second;
+
+            int start = max(0, curtime - x); //finding the start time (like queries[i]-x), cant be less than 0
+            int end = curtime;
+
+
+            while (j < m and vp[j].first <= end) //move j until the value of time in logs is not more than end
+            {
+                mp[vp[j].second]++;
+                j++;
+            }
+            while (i < m and vp[i].first < start) //move i until the value is not >= start
+            {
+                //removing out of window elements
+                if (mp[vp[i].second] == 1) mp.erase(vp[i].second); //if it is its only occurence then erase it.
+                else mp[vp[i].second]--;
+                i++;
+            }
+            ans[ind] = n - mp.size();
+        }
+        return ans;
+    }
+};
+
+int main() {
+    char c;
+    Solution sol;
+    //vector<int> asteroids = {5, 10, -5};
+    vector<int> starPos = { 1, 0 };
+    vector<int> homePos = { 2, 3 };
+    vector<int> rowCosts = { 5, 4, 3 };
+    vector<int> colCosts = { 8, 2, 6, 7 };
+
+    cout << sol.minCost(starPos, homePos, rowCosts, colCosts);
+
+
+    //cout << "Test  application";
+
+    cin >> c;
+}
