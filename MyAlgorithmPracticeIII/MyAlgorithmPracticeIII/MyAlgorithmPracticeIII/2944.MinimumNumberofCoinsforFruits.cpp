2dd9a6ab9707aac5fbff652ab2b0cd1eca9e7eac
bamtori <bamtori@hotmail.com>
2026-05-07 19:49:35 -0700

    1
    1

---
+++ MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/2944.MinimumNumberofCoinsforFruits.cpp
@@ -0,0 +1,53 @@
+// see answer : dp
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <stack>
+#include <bitset>
+#include <set>
+#include <list>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumCoins(vector<int>& A) {
+        int n = A.size();
+        vector<int> dp(n + 1, 0);
+        deque<int> q;
+        for (int i = 0; i < n; i++) {
+            while (!q.empty() && (q.front() + 1) * 2 < i + 1)
+                q.pop_front();
+            while (!q.empty() && dp[q.back()] + A[q.back()] >= dp[i] + A[i])
+                q.pop_back();
+            q.push_back(i);
+            dp[i + 1] = dp[q.front()] + A[q.front()];
+        }
+        return dp[n];
+    }
+};
+
+int main() {
+    char c;
+    Solution sol;
+
+    vector<int> numbers = { 2, 7, 11, 15 };
+
+    vector<int> ans = sol.twoSum(numbers, 9);
+
+    for (auto& it : ans) {
+        cout << it << " ";
+    }
+
+    cin >> c;
+}
