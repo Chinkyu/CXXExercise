5578f9ef05b213bb5a9fae776dcdad318c0370de
bamtori <bamtori@hotmail.com>
2026-07-08 19:20:27 -0700

    1
    1

--- MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/2592.MaximizeGreatnessofanArray.cpp
+++ MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/2592.MaximizeGreatnessofanArray.cpp
@@ -21,15 +21,18 @@ using namespace std;
 
 
 // 생각보다 너무 간단한데....
-int maximizeGreatness(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    int i = 0, j = 0, ans = 0;
-    while (j < nums.size()) {
-        if (nums[j] > nums[i]) { i++; ans++; }
-        j++;
+class Solution {
+public:
+    int maximizeGreatness(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = 0, j = 0, ans = 0;
+        while (j < nums.size()) {
+            if (nums[j] > nums[i]) { i++; ans++; }
+            j++;
+        }
+        return ans;
     }
-    return ans;
-}
+};
 
 
 // Map  으로 다시하자.. 
