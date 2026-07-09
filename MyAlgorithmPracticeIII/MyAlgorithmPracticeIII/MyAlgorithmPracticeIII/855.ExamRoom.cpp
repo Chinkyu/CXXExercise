a583e2830c8b63e20dc25ab3806eecb493cab0a1
bamtori <bamtori@hotmail.com>
2026-07-08 19:21:52 -0700

    1
    1

--- MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/855.ExamRoom.cpp
+++ MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/MyAlgorithmPracticeIII/855.ExamRoom.cpp
@@ -1,4 +1,4 @@
-// see answer
+// see answer : simple rather than expected 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -19,35 +19,39 @@
 
 using namespace std;
 
-int N;
-vector<int> L;
-ExamRoom(int n) {
-    N = n;
-}
+//let's do tomorrow 
+class ExamRoom {
+public:
+    int N;
+    vector<int> L;
+    ExamRoom(int n) {
+        N = n;
+    }
 
-int seat() {
-    if (L.size() == 0) {
-        L.push_back(0);
-        return 0;
-    }
-    int d = max(L[0], N - 1 - L[L.size() - 1]);
-    for (int i = 0; i < L.size() - 1; ++i) d = max(d, (L[i + 1] - L[i]) / 2);
-    if (L[0] == d) {
-        L.insert(L.begin(), 0);
-        return 0;
-    }
-    for (int i = 0; i < L.size() - 1; ++i)
-        if ((L[i + 1] - L[i]) / 2 == d) {
-            L.insert(L.begin() + i + 1, (L[i + 1] + L[i]) / 2);
-            return L[i + 1];
+    int seat() {
+        if (L.size() == 0) {
+            L.push_back(0);
+            return 0;
         }
-    L.push_back(N - 1);
-    return N - 1;
-}
+        int d = max(L[0], N - 1 - L[L.size() - 1]);
+        for (int i = 0; i < L.size() - 1; ++i) d = max(d, (L[i + 1] - L[i]) / 2);
+        if (L[0] == d) {
+            L.insert(L.begin(), 0);
+            return 0;
+        }
+        for (int i = 0; i < L.size() - 1; ++i)
+            if ((L[i + 1] - L[i]) / 2 == d) {
+                L.insert(L.begin() + i + 1, (L[i + 1] + L[i]) / 2);
+                return L[i + 1];
+            }
+        L.push_back(N - 1);
+        return N - 1;
+    }
 
-void leave(int p) {
-    for (int i = 0; i < L.size(); ++i) if (L[i] == p) L.erase(L.begin() + i);
-}
+    void leave(int p) {
+        for (int i = 0; i < L.size(); ++i) if (L[i] == p) L.erase(L.begin() + i);
+    }
+};
 
 /**
  * Your ExamRoom object will be instantiated and called as such:
